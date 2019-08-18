8bef2bba9064474db0a31bc0551f3d6354d92eb4
luhaojian11 <962671114@qq.com>
2019-08-18 10:38:54 +0800

    delete filteredReportDataList list because it is not necessary for the method

--- src/main/java/sales/SalesApp.java
+++ src/main/java/sales/SalesApp.java
@@ -1,6 +1,5 @@
 package sales;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -8,57 +7,37 @@ import java.util.List;
 public class SalesApp {
 
 	public void generateSalesActivityReport(String salesId, int maxRow, boolean isNatTrade, boolean isSupervisor) {
-		
-		SalesDao salesDao = new SalesDao();
+
+        SalesDao salesDao = new SalesDao();
 		SalesReportDao salesReportDao = new SalesReportDao();
 		List<String> headers = null;
-		
-		List<SalesReportData> filteredReportDataList = new ArrayList<SalesReportData>();
-		
-		if (salesId == null) {
+
+        if (salesId == null) {
 			return;
 		}
-		
-		Sales sales = salesDao.getSalesBySalesId(salesId);
-		
-		Date today = new Date();
+
+        Sales sales = salesDao.getSalesBySalesId(salesId);
+
+        Date today = new Date();
 		if (today.after(sales.getEffectiveTo())
 				|| today.before(sales.getEffectiveFrom())){
 			return;
 		}
-		
-		List<SalesReportData> reportDataList = salesReportDao.getReportData(sales);
-		
-		for (SalesReportData data : reportDataList) {
-			if ("SalesActivity".equalsIgnoreCase(data.getType())) {
-				if (data.isConfidential()) {
-					if (isSupervisor) {
-						filteredReportDataList.add(data);
-					}
-				}else {
-					filteredReportDataList.add(data);
-				}
-			}
-		}
-		
-		List<SalesReportData> tempList = new ArrayList<SalesReportData>();
-		for (int i=0; i < reportDataList.size() || i < maxRow; i++) {
-			tempList.add(reportDataList.get(i));
-		}
-		filteredReportDataList = tempList;
-		
-		if (isNatTrade) {
+
+        List<SalesReportData> reportDataList = salesReportDao.getReportData(sales);
+
+        if (isNatTrade) {
 			headers = Arrays.asList("Sales ID", "Sales Name", "Activity", "Time");
 		} else {
 			headers = Arrays.asList("Sales ID", "Sales Name", "Activity", "Local Time");
 		}
-		
-		SalesActivityReport report = this.generateReport(headers, reportDataList);
-		
-		EcmService ecmService = new EcmService();
+
+        SalesActivityReport report = this.generateReport(headers, reportDataList);
+
+        EcmService ecmService = new EcmService();
 		ecmService.uploadDocument(report.toXml());
-		
-	}
+
+    }
 
 	private SalesActivityReport generateReport(List<String> headers, List<SalesReportData> reportDataList) {
 		// TODO Auto-generated method stub
